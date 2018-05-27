9f880e0cd10ed1dfbebe6c78592141a3c76731b5
jamesl <james.laurenstin@bell.ca>
2018-05-27 02:35:47 -0400

    Fix String extension

--- NMFUI/Sources/Extensions/StringExtensions.swift
+++ NMFUI/Sources/Extensions/StringExtensions.swift
@@ -26,13 +26,14 @@ extension String {
     }
 }
 
+// MARK: - Spring validation
 extension String {
     /// Validate email string
     ///
     /// - parameter email: A String that rappresent an email address
     ///
     /// - returns: A Boolean value indicating whether an email is valid.
-    func isValid(_ email: String) -> Bool {
+    public func isValid(_ email: String) -> Bool {
         let emailRegEx = "(?:[a-zA-Z0-9!#$%\\&‘*+/=?\\^_`{|}~-]+(?:\\.[a-zA-Z0-9!#$%\\&'*+/=?\\^_`{|}" +
                          "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
                          "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-" +
